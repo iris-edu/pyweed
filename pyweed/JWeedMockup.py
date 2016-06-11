84c2bf5d8a1fc9b594f5a32df14054aff172ba15
Rowan Callahan <rowan.l.callahan@gmail.com>
2016-06-10 17:11:03 -0700

    Added in the new ui setup python file for the dialog box. Also added this new change to the JWEED mockup

--- pyweed/JWeedMockup.py
+++ pyweed/JWeedMockup.py
@@ -9,33 +9,27 @@ Created on Tue Jun  7 13:51:59 2016
 import sys
 from PyQt4 import QtGui,QtCore
 
+import practiceEventDialog
 
-
-class EventsDialog(QtGui.QDialog):
+class EventsDialog(QtGui.QDialog, practiceEventDialog.Ui_Dialog):
     """
     This is the event Dialog window, we will call it later in the main loop.
     """
     def __init__(self, parent=None, windowTitle='Start/End Time'):
-        super(EventsDialog, self).__init__(parent)
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
         
-        # create the ok/cancel button box
-        self.buttonBox = QtGui.QDialogButtonBox(self)
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Ok|QtGui.QDialogButtonBox.Cancel)
         
         # setup the date selectors
-        self.startTimeBox = QtGui.QDateTimeEdit(self)
-        self.startTimeBox.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
-        self.endTimeBox = QtGui.QDateTimeEdit(self)
-        self.endTimeBox.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
-        self.startTime = self.startTimeBox.dateTime()
-        self.endTime = self.endTimeBox.dateTime()
+        
+        self.endDateTimeEdit.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
+        self.startDateTimeEdit.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
+        
+        self.startTime = self.startDateTimeEdit.dateTime()
+        self.endTime = self.endDateTimeEdit.dateTime()
+        self.closeButton.clicked.connect(self.close)
 
-        # add the widgets to a vertical layour
-        self.verticalLayout = QtGui.QVBoxLayout(self)
-        self.verticalLayout.addWidget(self.startTimeBox)
-        self.verticalLayout.addWidget(self.endTimeBox)
-        self.verticalLayout.addWidget(self.buttonBox)
+
         self.setWindowTitle(windowTitle)
 
 class StationsDialog(QtGui.QDialog):
@@ -48,17 +42,17 @@ class StationsDialog(QtGui.QDialog):
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Ok|QtGui.QDialogButtonBox.Cancel)
         
         # setup the date selectors
-        self.startTimeBox = QtGui.QDateTimeEdit(self)
-        self.startTimeBox.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
-        self.endTimeBox = QtGui.QDateTimeEdit(self)
-        self.endTimeBox.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
-        self.startTime = self.startTimeBox.dateTime()
-        self.endTime = self.endTimeBox.dateTime()
+        self.startDateTimeEdit = QtGui.QDateTimeEdit(self)
+        self.startDateTimeEdit.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
+        self.endDateTimeEdit = QtGui.QDateTimeEdit(self)
+        self.endDateTimeEdit.setDisplayFormat('yyyy-MM-dd hh:mm:ss UTC')
+        self.startTime = self.startDateTimeEdit.dateTime()
+        self.endTime = self.endDateTimeEdit.dateTime()
 
         # add the widgets to a vertical layour
         self.verticalLayout = QtGui.QVBoxLayout(self)
-        self.verticalLayout.addWidget(self.startTimeBox)
-        self.verticalLayout.addWidget(self.endTimeBox)
+        self.verticalLayout.addWidget(self.startDateTimeEdit)
+        self.verticalLayout.addWidget(self.endDateTimeEdit)
         self.verticalLayout.addWidget(self.buttonBox)
         self.setWindowTitle(windowTitle)
 
@@ -89,7 +83,7 @@ class BrowserWindow(QtGui.QMainWindow):
         statePrintButtonsBox.addWidget(eventStateButton)
         statePrintButtonsBox.addWidget(stationStateButton)
         
-        
+        #
         eventStateList = QtGui.QListWidget()
         stationStateList = QtGui.QListWidget()
         statePrintBox = QtGui.QHBoxLayout()
@@ -102,7 +96,7 @@ class BrowserWindow(QtGui.QMainWindow):
         verticalInformationBox.addLayout(statePrintButtonsBox)
         self.informationLogFrame.setLayout(verticalInformationBox)
         
-        
+        #make the two halves splittable
         mapInformationSplitter = QtGui.QSplitter(QtCore.Qt.Vertical)
         mapInformationSplitter.addWidget(self.mainMapFrame)
         mapInformationSplitter.addWidget(self.informationLogFrame)
@@ -130,31 +124,31 @@ class BrowserWindow(QtGui.QMainWindow):
         fileMenu.addAction(stationsDialogAction)
       
         self.eventsDialogWindow = EventsDialog(self, 'Event Start/End Time')
-        self.eventsDialogWindow.endTimeBox.dateTimeChanged.connect(self.saveQueryState)
-        self.eventsDialogWindow.startTimeBox.dateTimeChanged.connect(self.saveQueryState)
+        self.eventsDialogWindow.endDateTimeEdit.dateTimeChanged.connect(self.saveQueryState)
+        self.eventsDialogWindow.startDateTimeEdit.dateTimeChanged.connect(self.saveQueryState)
         
         self.stationsDialogWindow = StationsDialog(self, 'Station Start/End Time')
-        self.stationsDialogWindow.startTimeBox.dateTimeChanged.connect(self.saveQueryState)
-        self.stationsDialogWindow.endTimeBox.dateTimeChanged.connect(self.saveQueryState)
+        self.stationsDialogWindow.startDateTimeEdit.dateTimeChanged.connect(self.saveQueryState)
+        self.stationsDialogWindow.endDateTimeEdit.dateTimeChanged.connect(self.saveQueryState)
         
         #set up all the initial settings for both the event dialog box and the staiton dialog box
         self.eventsDialogWindow.startTime = QtCore.QDateTime(2011,4,22,16,33,15)# for some reason this box wont let me set the date and time orriginally
         self.eventsDialogWindow.endTime = QtCore.QDateTime.currentDateTime()#sets the current date and time
-        self.eventsDialogWindow.endTimeBox.setDateTime(self.eventsDialogWindow.endTime)
-        self.eventsDialogWindow.startTimeBox.setDateTime(self.eventsDialogWindow.startTime)
+        self.eventsDialogWindow.endDateTimeEdit.setDateTime(self.eventsDialogWindow.endTime)
+        self.eventsDialogWindow.startDateTimeEdit.setDateTime(self.eventsDialogWindow.startTime)
         
         self.stationsDialogWindow.startTime = QtCore.QDateTime(2011,4,22,16,33,15)# for some reason this box wont let me set the date and time orriginally
         self.stationsDialogWindow.endTime = QtCore.QDateTime.currentDateTime()#sets the current date and time
-        self.stationsDialogWindow.endTimeBox.setDateTime(self.stationsDialogWindow.endTime)
-        self.stationsDialogWindow.startTimeBox.setDateTime(self.stationsDialogWindow.startTime)
+        self.stationsDialogWindow.endDateTimeEdit.setDateTime(self.stationsDialogWindow.endTime)
+        self.stationsDialogWindow.startDateTimeEdit.setDateTime(self.stationsDialogWindow.startTime)
         # set our window title and show our window when it is called
         self.setWindowTitle('Main window')    
         self.show()
    
     @QtCore.pyqtSlot()
     def openEventsDialog(self):
-        self.eventsDialogWindow.endTimeBox.setDateTime(self.eventsDialogWindow.endTime)
-        self.eventsDialogWindow.startTimeBox.setDateTime(self.eventsDialogWindow.startTime)        
+        self.eventsDialogWindow.endDateTimeEdit.setDateTime(self.eventsDialogWindow.endTime)
+        self.eventsDialogWindow.startDateTimeEdit.setDateTime(self.eventsDialogWindow.startTime)        
         self.eventsDialogWindow.exec_()
 
     @QtCore.pyqtSlot()
@@ -163,10 +157,10 @@ class BrowserWindow(QtGui.QMainWindow):
     
     @QtCore.pyqtSlot()
     def saveQueryState(self):
-        self.eventsDialogWindow.startTime = self.eventsDialogWindow.startTimeBox.dateTime()
-        self.eventsDialogWindow.endTime = self.eventsDialogWindow.endTimeBox.dateTime()
-        self.stationsDialogWindow.startTime = self.stationsDialogWindow.startTimeBox.dateTime()
-        self.stationsDialogWindow.endTime = self.stationsDialogWindow.endTimeBox.dateTime()                
+        self.eventsDialogWindow.startTime = self.eventsDialogWindow.startDateTimeEdit.dateTime()
+        self.eventsDialogWindow.endTime = self.eventsDialogWindow.endDateTimeEdit.dateTime()
+        self.stationsDialogWindow.startTime = self.stationsDialogWindow.startDateTimeEdit.dateTime()
+        self.stationsDialogWindow.endTime = self.stationsDialogWindow.endDateTimeEdit.dateTime()                
         
   #  def showEventState(self):
         
