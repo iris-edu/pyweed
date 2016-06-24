2306e7222d1ba011fb2b187cc3dc7d86cb4aca79
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2016-06-24 15:00:49 -0700

    basic stations query

--- pyweed/stations.py
+++ pyweed/stations.py
@@ -1,5 +1,5 @@
 """
-Container for events.
+Container for stations.
 
 :copyright:
     Mazama Science
@@ -15,9 +15,9 @@ import urllib
 import numpy as np
 import pandas as pd
 
-class Events(object):
+class Stations(object):
     """
-    Container for events.
+    Container for stations.
     """
     def __init__(self):
         """
@@ -39,13 +39,13 @@ class Events(object):
 
     def query(self, parameters=None):
         """
-        Make a webservice request for events using the passed in options
+        Make a webservice request for stations using the passed in options
         """
         # Sanity check
         if not parameters.has_key('starttime') or not parameters.has_key('endtime'):
             raise('starttime or endtime is missing')
                     
-        # Create events webservice URL
+        # Create stations webservice URL
         url = build_url(parameters=parameters)
         
         # Check if we have already gotten this url
@@ -55,9 +55,18 @@ class Events(object):
             
         else:  
             try:
-                # Get events dataframe and clean up column names
+                # Get stations dataframe and clean up column names
                 df = pd.read_csv(url, sep='|')
                 df.columns = df.columns.str.strip()
+                df.columns = df.columns.str.strip('#')   # take care of column named '#Network'
+                # Awkward conversion of 'Location' column from float to character
+                # NOTE:  Using [:,'colname'] syntax here because of the following warning:
+                # NOTE:  http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-view-versus-copy
+                df.loc[np.isnan(df['Location']),'Location'] = 999
+                df.loc[:,'Location'] = df.loc[:,'Location'].astype(int)
+                df.loc[:,'Location'] = df.loc[:,'Location'].astype(str)
+                df.loc[df['Location'] == '999','Location'] = '--'          # Could be represented as '' or '--'
+                df.loc[df['Location'] == '0','Location'] = '00'
                 # Push items onto the stack (so the most recent is always in position 0)
                 self.parameters_history.insert(0, parameters)
                 self.url_history.insert(0, url)
@@ -75,23 +84,25 @@ class Events(object):
 
 # Similar to obspy.clients.fdsn.client.py
 def build_url(base_url="http://service.iris.edu",
-              service="event",
+              service="station",
               major_version=1,
               resource_type="query",
               parameters={}):
     """
-    Build a FDSN webservice url to request events.
+    Build a FDSN webservice url to request stations.
 
-    >>> print(build_url("http://service.iris.edu", "event", 1, \
+    >>> print(build_url("http://service.iris.edu", "station", 1, \
                         "query", {'starttime':'2015-01-01','endtime':'2015-01-03','minmag':'4.00','format':'text'}))
-    http://service.iris.edu/fdsnws/event/1/query?endtime=2015-01-03&starttime=2015-01-01&minmag=4.00&format=text
+    http://service.iris.edu/fdsnws/station/1/query?endtime=2015-01-03&starttime=2015-01-01&minmag=4.00&format=text
     """
 
     # Construct base_url from FDSN provider, service, verstion.
     url = "/".join((base_url, "fdsnws", service,
                     str(major_version), resource_type))
     
-    # Aded a parameter for text formatting
+    # Add parameter for channel-level output
+    parameters['level'] = 'channel'
+    # Add parameter for text formatting
     parameters['format'] = 'text'
 
     # Add parameters
