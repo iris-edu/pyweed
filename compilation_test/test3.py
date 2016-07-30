78d70dbb6447db50a655950b49f06a65ab0e4303
Jonathan Callahan <jonathan@shuksan.domain>
2016-07-29 17:15:13 -0700

    adding compilation_test/ directory

---
+++ compilation_test/test3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function)
+
+import sys
+import string
+import urllib
+
+import numpy
+
+print("Hello world.")
+
---
+++ compilation_test/test5.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (absolute_import, division, print_function)
+
+import sys
+import string
+import urllib
+
+import numpy
+import pandas
+
+import PyQt4
+
+print("Hello world.")
+
