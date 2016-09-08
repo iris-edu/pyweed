46a8c066afbbee7733c70fc41c3682ce00e9f3e8
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2016-09-07 17:10:05 -0700

    harmonize naming
     * events.py --> eventsHandler.py
    
     * stations.py --> stationsHandler.py
    
     * camelCase within pyweed_gui.py (to match Qt)
    
     * snake_case within sub-modules (to match ObsPy)

--- pyweed/eventsHandler.py
+++ pyweed/eventsHandler.py
@@ -1,5 +1,5 @@
 """
-Container for stations.
+Container for events.
 
 :copyright:
     Mazama Science
@@ -15,9 +15,9 @@ import urllib
 import numpy as np
 import pandas as pd
 
-class Stations(object):
+class EventsHandler(object):
     """
-    Container for stations.
+    Container for events.
     """
     def __init__(self):
         """
@@ -34,58 +34,50 @@ class Stations(object):
     def get_url(self, index=0):
         return(self.url_history[index])
     
-    def loadData(self, parameters=None):
+    def load_data(self, parameters=None):
         """
-        Make a webservice request for stations using the passed in options
+        Make a webservice request for events using the passed in options
         """
         # Sanity check
         if not parameters.has_key('starttime') or not parameters.has_key('endtime'):
             raise('starttime or endtime is missing')
                     
-        # Create stations webservice URL
+        # Create events webservice URL
         url = build_url(parameters=parameters, output_format="text")
-        
+                    
         try:
-            # Get stations dataframe and clean up column names
+            # Get events dataframe and clean up column names
             df = pd.read_csv(url, sep='|')
             df.columns = df.columns.str.strip()
-            df.columns = df.columns.str.strip('#')   # take care of column named '#Network'
-            # Awkward conversion of 'Location' column from float to character
-            # NOTE:  Using [:,'colname'] syntax here because of the following warning:
-            # NOTE:  http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-view-versus-copy
-            df.loc[np.isnan(df['Location']),'Location'] = 999
-            df.loc[:,'Location'] = df.loc[:,'Location'].astype(int)
-            df.loc[:,'Location'] = df.loc[:,'Location'].astype(str)
-            df.loc[df['Location'] == '999','Location'] = '--'          # Could be represented as '' or '--'
-            df.loc[df['Location'] == '0','Location'] = '00'
-            # Add 'SNCL' column
-            sncls = df.apply(lambda x: '%s.%s.%s.%s' % (x['Network'],x['Station'],x['Location'],x['Channel']), axis=1)
-            df['SNCL'] = sncls.tolist()
-            df = df[self.get_columnNames()]
+            df.columns = df.columns.str.lstrip('#') # remove '#' from '#EventID'
+            # Rearrange columns
+            df = df[self.get_column_names()]
+            # Convert EventID to character
+            df['EventID'] = df['EventID'].astype(str)
             # Push items onto the stack (so the most recent is always in position 0)
             self.url_history.insert(0, url)
         except:
             # TODO:  What type of exception should we trap? We should probably log it.
             raise
-
+            
         self.currentDF = df
-
-        return(df)
         
-    def get_columnNames(self):
-        columnNames = ['Network', 'Station', 'Location', 'Channel', 'Longitude', 'Latitude', 'Elevation', 'Depth', 'Azimuth', 'Dip', 'SensorDescription', 'Scale', 'ScaleFreq', 'ScaleUnits', 'SampleRate', 'StartTime', 'EndTime', 'SNCL']
-        return(columnNames)
+        return(df)
 
-    def get_selectedIDs(self):
+    def get_column_names(self):
+        columnNames = ['Time', 'Magnitude', 'Longitude', 'Latitude', 'Depth/km', 'MagType', 'EventLocationName', 'Author', 'Catalog', 'Contributor', 'ContributorID', 'MagAuthor', 'EventID']
+        return(columnNames)
+    
+    def get_selected_ids(self):
         return(self.selectedIDs)
     
-    def set_selectedIDs(self, IDs):
+    def set_selected_ids(self, IDs):
         self.selectedIDs = IDs
         
-    def get_selectedDataframe(self):
-        df = self.currentDF.loc[self.currentDF['SNCL'].isin(self.selectedIDs)]
+    def get_selected_dataframe(self):
+        df = self.currentDF.loc[self.currentDF['EventID'].isin(self.selectedIDs)]
         return(df)
-    
+        
         
 # ------------------------------------------------------------------------------
 # Helper functions
@@ -93,27 +85,24 @@ class Stations(object):
 
 # Similar to obspy.clients.fdsn.client.py
 def build_url(base_url="http://service.iris.edu",
-              service="station",
+              service="event",
               major_version=1,
               resource_type="query",
               parameters={},
               output_format="xml"):
     """
-    Build a FDSN webservice url to request stations.
+    Build a FDSN webservice url to request events.
 
-    >>> print(build_url("http://service.iris.edu", "station", 1, \
+    >>> print(build_url("http://service.iris.edu", "event", 1, \
                         "query", {'starttime':'2015-01-01','endtime':'2015-01-03','minmag':'4.00'}, "text"))
-    http://service.iris.edu/fdsnws/station/1/query?endtime=2015-01-03&starttime=2015-01-01&minmag=4.00&format=text
+    http://service.iris.edu/fdsnws/event/1/query?endtime=2015-01-03&starttime=2015-01-01&minmag=4.00&format=text
     """
 
     # Construct base_url from FDSN provider, service, verstion.
     url = "/".join((base_url, "fdsnws", service,
                     str(major_version), resource_type))
     
-    # Add parameter for channel-level output
-    parameters['level'] = 'channel'
-    
-    # Aded a parameter for the output format -- either 'xml' (used to generate an ObsPy inventory) or 'text' (used to generate a pandas dataframe)
+    # Aded a parameter for the output format -- either 'xml' (used to generate an ObsPy catalog) or 'text' (used to generate a pandas dataframe)
     parameters['format'] = output_format
 
     # Add parameters
--- pyweed/stationsHandler.py
+++ pyweed/stationsHandler.py
@@ -1,5 +1,5 @@
 """
-Container for events.
+Container for stations.
 
 :copyright:
     Mazama Science
@@ -15,9 +15,9 @@ import urllib
 import numpy as np
 import pandas as pd
 
-class Events(object):
+class StationsHandler(object):
     """
-    Container for events.
+    Container for stations.
     """
     def __init__(self):
         """
@@ -34,50 +34,58 @@ class Events(object):
     def get_url(self, index=0):
         return(self.url_history[index])
     
-    def loadData(self, parameters=None):
+    def load_data(self, parameters=None):
         """
-        Make a webservice request for events using the passed in options
+        Make a webservice request for stations using the passed in options
         """
         # Sanity check
         if not parameters.has_key('starttime') or not parameters.has_key('endtime'):
             raise('starttime or endtime is missing')
                     
-        # Create events webservice URL
+        # Create stations webservice URL
         url = build_url(parameters=parameters, output_format="text")
-                    
+        
         try:
-            # Get events dataframe and clean up column names
+            # Get stations dataframe and clean up column names
             df = pd.read_csv(url, sep='|')
             df.columns = df.columns.str.strip()
-            df.columns = df.columns.str.lstrip('#') # remove '#' from '#EventID'
-            # Rearrange columns
-            df = df[self.get_columnNames()]
-            # Convert EventID to character
-            df['EventID'] = df['EventID'].astype(str)
+            df.columns = df.columns.str.strip('#')   # take care of column named '#Network'
+            # Awkward conversion of 'Location' column from float to character
+            # NOTE:  Using [:,'colname'] syntax here because of the following warning:
+            # NOTE:  http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-view-versus-copy
+            df.loc[np.isnan(df['Location']),'Location'] = 999
+            df.loc[:,'Location'] = df.loc[:,'Location'].astype(int)
+            df.loc[:,'Location'] = df.loc[:,'Location'].astype(str)
+            df.loc[df['Location'] == '999','Location'] = '--'          # Could be represented as '' or '--'
+            df.loc[df['Location'] == '0','Location'] = '00'
+            # Add 'SNCL' column
+            sncls = df.apply(lambda x: '%s.%s.%s.%s' % (x['Network'],x['Station'],x['Location'],x['Channel']), axis=1)
+            df['SNCL'] = sncls.tolist()
+            df = df[self.get_column_names()]
             # Push items onto the stack (so the most recent is always in position 0)
             self.url_history.insert(0, url)
         except:
             # TODO:  What type of exception should we trap? We should probably log it.
             raise
-            
-        self.currentDF = df
-        
-        return(df)
 
-    def get_columnNames(self):
-        columnNames = ['Time', 'Magnitude', 'Longitude', 'Latitude', 'Depth/km', 'MagType', 'EventLocationName', 'Author', 'Catalog', 'Contributor', 'ContributorID', 'MagAuthor', 'EventID']
+        self.currentDF = df
+
+        return(df)
+        
+    def get_column_names(self):
+        columnNames = ['Network', 'Station', 'Location', 'Channel', 'Longitude', 'Latitude', 'Elevation', 'Depth', 'Azimuth', 'Dip', 'SensorDescription', 'Scale', 'ScaleFreq', 'ScaleUnits', 'SampleRate', 'StartTime', 'EndTime', 'SNCL']
         return(columnNames)
-    
-    def get_selectedIDs(self):
+
+    def get_selected_ids(self):
         return(self.selectedIDs)
     
-    def set_selectedIDs(self, IDs):
+    def set_selected_ids(self, IDs):
         self.selectedIDs = IDs
         
-    def get_selectedDataframe(self):
-        df = self.currentDF.loc[self.currentDF['EventID'].isin(self.selectedIDs)]
+    def get_selected_dataframe(self):
+        df = self.currentDF.loc[self.currentDF['SNCL'].isin(self.selectedIDs)]
         return(df)
-        
+    
         
 # ------------------------------------------------------------------------------
 # Helper functions
@@ -85,24 +93,27 @@ class Events(object):
 
 # Similar to obspy.clients.fdsn.client.py
 def build_url(base_url="http://service.iris.edu",
-              service="event",
+              service="station",
               major_version=1,
               resource_type="query",
               parameters={},
               output_format="xml"):
     """
-    Build a FDSN webservice url to request events.
+    Build a FDSN webservice url to request stations.
 
-    >>> print(build_url("http://service.iris.edu", "event", 1, \
+    >>> print(build_url("http://service.iris.edu", "station", 1, \
                         "query", {'starttime':'2015-01-01','endtime':'2015-01-03','minmag':'4.00'}, "text"))
-    http://service.iris.edu/fdsnws/event/1/query?endtime=2015-01-03&starttime=2015-01-01&minmag=4.00&format=text
+    http://service.iris.edu/fdsnws/station/1/query?endtime=2015-01-03&starttime=2015-01-01&minmag=4.00&format=text
     """
 
     # Construct base_url from FDSN provider, service, verstion.
     url = "/".join((base_url, "fdsnws", service,
                     str(major_version), resource_type))
     
-    # Aded a parameter for the output format -- either 'xml' (used to generate an ObsPy catalog) or 'text' (used to generate a pandas dataframe)
+    # Add parameter for channel-level output
+    parameters['level'] = 'channel'
+    
+    # Aded a parameter for the output format -- either 'xml' (used to generate an ObsPy inventory) or 'text' (used to generate a pandas dataframe)
     parameters['format'] = output_format
 
     # Add parameters
