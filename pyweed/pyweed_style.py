9983a65838491260082b4560319cd05adac61aee
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2016-11-12 13:24:01 -0800

    improved styling for the WaveformsDialog

--- pyweed/pyweed_style.py
+++ pyweed/pyweed_style.py
@@ -8,27 +8,77 @@ Qt Stylesheet.
     (http://www.gnu.org/copyleft/lesser.html)
 """
 
+# Qt StyleSheet
+#
+# See:  http://doc.qt.io/qt-4.8/stylesheet-reference.html
+# See:  https://github.com/ColinDuquesnoy/QDarkStyleSheet/blob/master/qdarkstyle/style.qss
+
 from __future__ import (absolute_import, division, print_function)
 
 stylesheet = """
 
+/* See:   */
+
+/* ----- Default Settings ----------------------------------------------------*/
+
+/* Tighten up all tables and remove visible gridlines */
+
+QTableView::item {
+    margin: 0px;
+    padding: 2px
+}
 
 QTableView {
     gridline-color: white;
 }
 
-QTableView::item {
-    margin 0px;
-    padding: 2px
+
+/* Tighten up all frames */
+
+QFrame {
+    margin: 0px;
+    padding: 0px;
 }
 
 
-/* Waveforms table -----------------------------------------------------------*/
+/* ----- Waveforms Dialog ----------------------------------------------------*/
 
 QDialog#WaveformDialog QTableView#selectionTable {
     selection-background-color: white;
     selection-color: black;
 }
+
+
+/* Prevent WaveformDialog.directoryPushButton from staying highlighted */
+
+QDialog#WaveformDialog directoryPushButton:focus {
+    background-color: white;
+}
+
+/* Remove border from frames */
+
+QDialog#WaveformDialog QFrame {
+    margin: 0px;
+    padding: 0px;
+    border: 0px none black;
+}
+
+/*
+QDialog#WaveformDialog QFrame#saveFrame {
+    margin: 0px;
+    padding: 0px;
+    border: 0px none black;
+}
+
+QDialog#WaveformDialog QFrame#saveFrame > QFrame {
+    margin: 0px;
+    padding: 0px;
+    border: 0px none black;
+}
+*/
+
+
+
 
 
 /* Example
@@ -60,11 +110,5 @@ QPushButton:focus {
     background-color: red;
 }
 */
-
-/* Prevent WaveformDialog.directoryPushButton from staying highlighted */
-QDialog#WaveformDialog directoryPushButton:focus {
-    background-color: white;
-}
-
 
 """
