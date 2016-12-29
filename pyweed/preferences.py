2bf94b3d5e7ecb060f6086643669594c8ceb5bcd
Adam Clark <adam@iris.washington.edu>
2016-12-28 16:37:07 -0800

    More preferences changes

--- pyweed/preferences.py
+++ pyweed/preferences.py
@@ -26,11 +26,12 @@ class Section(object):
 
     def read_from_config(self, config):
         name = self.__name__
-        for k in self.__dict__.keys():
-            if config.has_option(name, k):
-                self.__dict__[k] = config.get(name, k)
-#         for k, v in initial.iteritems():
-#             setattr(self, k, v)
+        if config.has_section(name):
+            fix_case = dict((k.lower(), k) for k in self.__dict__.keys())
+            for k, v in config.items(name):
+                if k in fix_case:
+                    k = fix_case[k]
+                self.__dict__[k] = v
 
     def write_to_config(self, config):
         name = self.__name__
@@ -43,7 +44,7 @@ class Section(object):
         return self.__class__.__name__
 
     def items(self):
-        return [i for i in self.__dict__.iteritems() if not i[0].startswith('__')]
+        return self.__dict__.items()
 
     def update(self, values):
         self.__dict__.update(values)
@@ -56,18 +57,6 @@ class Section(object):
         return type(section_name, (cls,), {})(**initial)
 
 
-DEFAULTS = {
-    "App": {
-        "name": __appName__,
-        "version": __version__,
-    },
-    "Waveforms": {
-        "downloadDir": os.path.join(os.path.expanduser("~"), ".pyweed_downloads"),
-        "cacheSize": "50", # megabytes
-    },
-}
-
-
 class Preferences(object):
     """
     Container for application preferences.
@@ -78,16 +67,24 @@ class Preferences(object):
         Initialization with default settings.
         """
 
-        for (section, prefs) in DEFAULTS.items():
-            setattr(self, section, Section.create(section, **prefs))
+#         for (section, prefs) in DEFAULTS.items():
+#             setattr(self, section, Section.create(section, **prefs))
 
-        self.Logging = type("Logging", (object,), {})()
+        self.App = Section.create("App")
+        self.App.name = __appName__
+        self.App.version = __version__
+
+        self.Waveforms = Section.create("Waveforms")
+        self.Waveforms.downloadDir = os.path.join(os.path.expanduser("~"), ".pyweed_downloads")
+        self.Waveforms.cacheSize = "50" # megabytes
+
+        self.Logging = Section.create("Logging")
         self.Logging.level = "DEBUG"
 
-        self.Map = type("Map", (object,), {})()
+        self.Map = Section.create("Map")
         self.Map.projection = "robin"
 
-        self.EventOptions = type("EventOptions", (object,), {})()
+        self.EventOptions = Section.create("EventOptions")
         self.EventOptions.minmag = "5.0"
         self.EventOptions.maxmag = "10.0"
         self.EventOptions.mindepth = "0.0"
@@ -103,7 +100,7 @@ class Preferences(object):
         self.EventOptions.selectedTimeButton = 'timeBetweenRadioButton' # or 'timeDuringStationsRadioButton'
         self.EventOptions.selectedLocationButton = 'locationRangeRadioButton' # or 'locationDistanceFromPointRadioButton' or 'locationDistanceFromStationsRadioButton'
 
-        self.StationOptions = type("StationOptions", (object,), {})()
+        self.StationOptions = Section.create("StationOptions")
         self.StationOptions.network = "_GSN"
         self.StationOptions.station = "*"
         self.StationOptions.location = "*"
@@ -125,27 +122,15 @@ class Preferences(object):
         """
         config = ConfigParser.SafeConfigParser()
 
-        for section in [self.Waveforms,]:
-            section_name = section.__class__.__name__
-            config.add_section(section_name)
-            for key, value in section:
-                config.set(section_name, key, str(value))
-
-        config.add_section("Logging")
-        for key, value in vars(self.Logging).items():
-            config.set("Logging", key, str(value))
-
-        config.add_section("Map")
-        for key, value in vars(self.Map).items():
-            config.set("Map", key, str(value))
-
-        config.add_section("EventOptions")
-        for key, value in vars(self.EventOptions).items():
-            config.set("EventOptions", key, str(value))
-
-        config.add_section("StationOptions")
-        for key, value in vars(self.StationOptions).items():
-            config.set("StationOptions", key, str(value))
+        sections = [
+            self.Waveforms,
+            self.Logging,
+            self.Map,
+            self.EventOptions,
+            self.StationOptions,
+        ]
+        for section in sections:
+            section.write_to_config(config)
 
         if not os.path.exists(user_config_path()):
             try:
@@ -176,53 +161,15 @@ class Preferences(object):
             config = ConfigParser.SafeConfigParser()
             config.readfp(f)
 
-            self.App = Preference("App", config=config)
-
-            # Read in preferences by section
-            self.set_option(config, 'Waveforms', 'downloadDir')
-            self.set_option(config, 'Waveforms', 'cacheSize')
-
-            self.set_option(config, 'Logging', 'logLevel')
-
-            self.set_option(config, 'Map', 'projection')
-
-            self.set_option(config, 'EventOptions', 'minmag')
-            self.set_option(config, 'EventOptions', 'maxmag')
-            self.set_option(config, 'EventOptions', 'mindepth')
-            self.set_option(config, 'EventOptions', 'maxdepth')
-            self.set_option(config, 'EventOptions', 'locationRangeWest')
-            self.set_option(config, 'EventOptions', 'locationRangeEast')
-            self.set_option(config, 'EventOptions', 'locationRangeSouth')
-            self.set_option(config, 'EventOptions', 'locationRangeNorth')
-            self.set_option(config, 'EventOptions', 'distanceFromPointMinRadius')
-            self.set_option(config, 'EventOptions', 'distanceFromPointMaxRadius')
-            self.set_option(config, 'EventOptions', 'distanceFromPointEast')
-            self.set_option(config, 'EventOptions', 'distanceFromPointNorth')
-            self.set_option(config, 'EventOptions', 'selectedTimeButton')
-            self.set_option(config, 'EventOptions', 'selectedLocationButton')
-
-            self.set_option(config, 'StationOptions', 'network')
-            self.set_option(config, 'StationOptions', 'station')
-            self.set_option(config, 'StationOptions', 'location')
-            self.set_option(config, 'StationOptions', 'channel')
-            self.set_option(config, 'StationOptions', 'locationRangeWest')
-            self.set_option(config, 'StationOptions', 'locationRangeEast')
-            self.set_option(config, 'StationOptions', 'locationRangeSouth')
-            self.set_option(config, 'StationOptions', 'locationRangeNorth')
-            self.set_option(config, 'StationOptions', 'distanceFromPointMinRadius')
-            self.set_option(config, 'StationOptions', 'distanceFromPointMaxRadius')
-            self.set_option(config, 'StationOptions', 'distanceFromPointEast')
-            self.set_option(config, 'StationOptions', 'distanceFromPointNorth')
-            self.set_option(config, 'StationOptions', 'selectedTimeButton')
-            self.set_option(config, 'StationOptions', 'selectedLocationButton')
-
-    def set_option(self, config, sectionName, name):
-        """Set a property on a named opreference object"""
-        try:
-            obj = getattr(self, sectionName)
-            setattr(obj, name, config.get(sectionName, name))
-        except ConfigParser.NoOptionError:
-            pass
+            sections = [
+                self.Waveforms,
+                self.Logging,
+                self.Map,
+                self.EventOptions,
+                self.StationOptions,
+            ]
+            for section in sections:
+                section.read_from_config(config)
 
 
 # ------------------------------------------------------------------------------
