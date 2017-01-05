5955f590107c78326c2f21f7c426a59a1ade5880
Adam Clark <adam@iris.washington.edu>
2017-01-04 17:08:51 -0800

    Handle occasionally missing event origins (see https://github.com/obspy/obspy/issues/1629)

--- pyweed/events_handler.py
+++ pyweed/events_handler.py
@@ -115,8 +115,14 @@ class EventsDataFrameLoader(SignalingThread):
         df = pd.DataFrame(columns=self.column_names)
 
         for event in self.event_catalog:
-            origin = event.preferred_origin()
-            magnitude = event.preferred_magnitude()
+            origin = event.preferred_origin() or (len(event.origins) and event.origins[0])
+            if not origin:
+                LOGGER.error("No preferred origin found for event %s", event)
+                continue
+            magnitude = event.preferred_magnitude() or (len(event.magnitudes) and event.magnitudes[0])
+            if not magnitude:
+                LOGGER.error("No preferred magnitude found for event %s", event)
+                continue
 
             # Uniformly handled elements
             time = origin.time.strftime("%Y-%m-%d %H:%M:%S") # use strftime to remove milliseconds
