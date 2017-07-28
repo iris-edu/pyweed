4a9001097fe829344ace810522b30bede72598c7
Adam Clark <adam@iris.washington.edu>
2017-07-28 16:19:55 -0700

    Add local path to the launcher script if needed #5739

--- pyweed_launcher.py
+++ pyweed_launcher.py
@@ -16,19 +16,29 @@ library imports required for the application itself.
 from __future__ import (absolute_import, division, print_function)
 
 import sys
+import os
 
 from future.utils import PY2
 if PY2:
     # Configure PyQt4 -- in order for the Python console to work in Python 2, we need to load a particular
     # version of some internal libraries. This must be done before the first import of the PyQt4 libraries.
     # See http://stackoverflow.com/questions/11513132/embedding-ipython-qt-console-in-a-pyqt-application/20610786#20610786
-    import os
     os.environ['QT_API'] = 'pyqt'
     import sip
     sip.setapi("QString", 2)
     sip.setapi("QVariant", 2)
 
 from PyQt4 import QtGui
+
+# This will usually be used to launch pyweed from the source code, so if pyweed
+# isn't in the path then add the source.
+try:
+    from pyweed import __version__
+except Exception as e:
+    sys.path.append(os.path.dirname(__file__))
+    from pyweed import __version__
+print("PyWEED v.%s" % __version__)
+
 from pyweed.gui.SplashScreenHandler import SplashScreenHandler
 import pyweed.gui.qrc
 
