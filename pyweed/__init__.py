23048f21af7523c67766f76c98768ed553cbd6cb
Adam Clark <adam@iris.washington.edu>
2017-12-11 16:42:10 -0800

    Bump version

--- pyweed/__init__.py
+++ pyweed/__init__.py
@@ -1,5 +1,7 @@
 import os.path
 
 __pkg_path___ = os.path.dirname(os.path.abspath(__file__))
-__version__ = '1.0.0b2'
+# Use Python semantic versioning
+# https://packaging.python.org/tutorials/distributing-packages/#semantic-versioning-preferred
+__version__ = '1.0.0'
 __app_name__ = "PyWEED"
