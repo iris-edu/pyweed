964c14ff3368b4d98f6531633e56cc99769e4dd1
Adam Clark <adam@iris.washington.edu>
2020-06-11 12:14:07 -0700

    Protect against failed ipython library, this leaves the Console dialog disabled but doesn't crash at least

--- pyweed/gui/ConsoleDialog.py
+++ pyweed/gui/ConsoleDialog.py
@@ -9,31 +9,40 @@ Dialog containing an IPython console.
     (http://www.gnu.org/copyleft/lesser.html)
 """
 
-from qtconsole.rich_ipython_widget import RichIPythonWidget
-from qtconsole.inprocess import QtInProcessKernelManager
 from PyQt5 import QtWidgets
 from pyweed.gui.BaseDialog import BaseDialog
+from logging import getLogger
 
+LOGGER = getLogger(__name__)
 
-class EmbedIPython(RichIPythonWidget):
+try:
+    from qtconsole.rich_ipython_widget import RichIPythonWidget
+    from qtconsole.inprocess import QtInProcessKernelManager
 
-    def __init__(self, **kwarg):
-        super(RichIPythonWidget, self).__init__()
-        self.kernel_manager = QtInProcessKernelManager()
-        self.kernel_manager.start_kernel()
-        self.kernel = self.kernel_manager.kernel
-        self.kernel.gui = 'qt4'
-        self.kernel.shell.push(kwarg)
-        self.kernel_client = self.kernel_manager.client()
-        self.kernel_client.start_channels()
+    class EmbedIPython(RichIPythonWidget):
+
+        def __init__(self, **kwarg):
+            super(RichIPythonWidget, self).__init__()
+            self.kernel_manager = QtInProcessKernelManager()
+            self.kernel_manager.start_kernel()
+            self.kernel = self.kernel_manager.kernel
+            self.kernel.gui = 'qt4'
+            self.kernel.shell.push(kwarg)
+            self.kernel_client = self.kernel_manager.client()
+            self.kernel_client.start_channels()
+
+except Exception as e:
+    LOGGER.error("Failed to make IPython widget: %s", e)
+    EmbedIPython = None
 
 
 class ConsoleDialog(BaseDialog):
     def __init__(self, pyweed, parent=None):
         super(ConsoleDialog, self).__init__(parent=parent)
-        self.widget = EmbedIPython(pyweed=pyweed)
-        layout = QtWidgets.QVBoxLayout(self)
-        layout.addWidget(self.widget)
+        if EmbedIPython:
+            self.widget = EmbedIPython(pyweed=pyweed)
+            layout = QtWidgets.QVBoxLayout(self)
+            layout.addWidget(self.widget)
 
 
 if __name__ == '__main__':
