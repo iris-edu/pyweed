72f0e31ab8e9ae2095635bb24369c1db71be8f51
Adam Clark <adam.clark@earthscope.org>
2025-07-21 11:30:23 -0700

    chore: bump version

--- pyweed/__init__.py
+++ pyweed/__init__.py
@@ -3,5 +3,5 @@ import os.path
 __pkg_path___ = os.path.dirname(os.path.abspath(__file__))
 # Use Python semantic versioning
 # https://packaging.python.org/tutorials/distributing-packages/#semantic-versioning-preferred
-__version__ = '1.0.10'
+__version__ = "1.0.11"
 __app_name__ = "PyWEED"
