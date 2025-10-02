3da8cd0b63fe7687ee14fc1f01b803f59d2691c3
Adam Clark <adam.clark@earthscope.org>
2025-10-02 10:53:32 -0700

    chore: prettify

--- pyweed/pyweed_launcher.py
+++ pyweed/pyweed_launcher.py
@@ -13,7 +13,7 @@ library imports required for the application itself.
     (http://www.gnu.org/copyleft/lesser.html)
 """
 
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
 
 import sys
 import os
@@ -21,29 +21,29 @@ import matplotlib
 import platform
 
 # Windows needs different treatement in a few places
-is_windows = (platform.system() == 'Windows')
+is_windows = platform.system() == "Windows"
 
 # If running under windows, redirect stdout/stderr, since writing to them will crash Python if there's
 # not a console. See https://bugs.python.org/issue706263
 if is_windows:
-    nul = open(os.devnull, 'w')
+    nul = open(os.devnull, "w")
     sys.stderr = nul
     sys.stdout = nul
 
 # Set up the PROJ_LIB environment variable needed by matplotlib
 # See https://github.com/conda-forge/basemap-feedstock/issues/30
-if not os.environ.get('PROJ_LIB'):
+if not os.environ.get("PROJ_LIB"):
     # Look for a prefix from Conda or use the standard one
-    prefix = os.environ.get('CONDA_PREFIX') or sys.prefix
+    prefix = os.environ.get("CONDA_PREFIX") or sys.prefix
     if prefix:
         # Location differs between platforms
         if is_windows:
-            os.environ['PROJ_LIB'] = os.path.join(prefix, 'Library', 'share', 'proj')
+            os.environ["PROJ_LIB"] = os.path.join(prefix, "Library", "share", "proj")
         else:
-            os.environ['PROJ_LIB'] = os.path.join(prefix, 'share', 'proj')
+            os.environ["PROJ_LIB"] = os.path.join(prefix, "share", "proj")
 
 # Configure matplotlib backend
-matplotlib.use('AGG')
+matplotlib.use("AGG")
 
 # For debugging, raise an exception on attempted chained assignment
 # See http://pandas.pydata.org/pandas-docs/version/0.19.1/indexing.html#returning-a-view-versus-a-copy
@@ -52,14 +52,18 @@ matplotlib.use('AGG')
 
 
 def init_strptime():
-    """ Workaround for https://github.com/obspy/obspy/issues/2147 """
+    """Workaround for https://github.com/obspy/obspy/issues/2147"""
     try:
         import locale
+
         # locale.setlocale(locale.LC_TIME, ('en_US', 'UTF-8'))
         from obspy.clients.fdsn.routing.routing_client import RoutingClient  # NOQA
-        locale.setlocale(locale.LC_TIME, '')
+
+        locale.setlocale(locale.LC_TIME, "")
     except Exception:
         pass
+
+
 init_strptime()
 
 
@@ -68,6 +72,7 @@ def fix_locale():
     Fix for issues with locale-bound number handling, we basically force decimals to use '.' rather than ','
     """
     from PyQt5.QtCore import QLocale
+
     QLocale.setDefault(QLocale.c())
 
 
@@ -76,6 +81,7 @@ def get_pyweed():
     Load the PyWEED GUI code, this is where most of the expensive stuff happens
     """
     from pyweed.gui.PyWeedGUI import PyWeedGUI
+
     return PyWeedGUI()
 
 
